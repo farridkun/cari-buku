d9de833bdac65d591c16375ed539ee72c35f422d
Farrid Kuntoro <farridkuntoro@email.unikom.ac.id>
2019-10-16 03:33:58 +0700

    penambahan fungsi

---
+++ app/Http/Controllers/CariBukuController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class CariBukuController extends Controller
+{
+    function index(Request $request)
+    {
+        if (request()->ajax()) {
+            if (!empty($request->filter_penulis)) {
+                $data = DB::table('tbl_buku')
+                    ->select('nama_buku', 'penulis', 'tahun_terbit')
+                    ->where('penulis', $request->filter_penulis)
+                    ->where('tahun_terbit', $request->filter_terbit)
+                    ->get();
+            } else {
+                $data = DB::table('tbl_buku')
+                    ->select('nama_buku', 'penulis', 'tahun_terbit')
+                    ->get();
+            }
+            return datatables()->of($data)->make(true);
+        }
+        $angka_tahun = DB::table('tbl_buku')
+            ->select('tahun_terbit')
+            ->groupBy('tahun_terbit')
+            ->orderBy('tahun_terbit', 'ASC')
+            ->get();
+        return view('caribuku', compact('angka_tahun'));
+    }
+}
---
+++ resources/views/caribuku.blade.php
@@ -0,0 +1,119 @@
+<html>
+ <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Data Buku Perpustakaan Jalanan</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <div class="container">    
+     <br />
+     <h3 align="center">Data Buku Perpustakaan Jalanan</h3>
+     <br />
+            <br />
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <select name="filter_penulis" id="filter_penulis" class="form-control" required>
+                            <option value="">Pilih Penulis</option>
+                            <option value="Js. Khairen">Js. Khairen</option>
+                            <option value="Febriani Ad">Febriani Ad</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <select name="filter_terbit" id="filter_terbit" class="form-control" required>
+                            <option value="">Pilih Tahun Terbit</option>
+                            @foreach($angka_tahun as $tahun_terbit)
+
+                            <option value="{{ $tahun_terbit->tahun_terbit }}">{{ $tahun_terbit->tahun_terbit }}</option>
+
+                            @endforeach
+                        </select>
+                    </div>
+                    
+                    <div class="form-group" align="center">
+                        <button type="button" name="filter" id="filter" class="btn btn-info">Filter</button>
+
+                        <button type="button" name="reset" id="reset" class="btn btn-default">Reset</button>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+            </div>
+            <br />
+   <div class="table-responsive">
+    <table id="data_buku" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nama Buku</th>
+                            <th>Penulis</th>
+                            <th>Tahun Terbit</th>
+                        </tr>
+                    </thead>
+                </table>
+   </div>
+            <br />
+            <br />
+  </div>
+ </body>
+</html>
+
+<script>
+$(document).ready(function(){
+
+    fill_datatable();
+
+    function fill_datatable(filter_penulis = '', filter_terbit = '')
+    {
+        var dataTable = $('#data_buku').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax:{
+                url: "{{ route('caribuku.index') }}",
+                data:{filter_penulis:filter_penulis, filter_terbit:filter_terbit}
+            },
+            columns: [
+                {
+                    data:'nama_buku',
+                    name:'nama_buku'
+                },
+                {
+                    data:'penulis',
+                    name:'penulis'
+                },
+                {
+                    data:'tahun_terbit',
+                    name:'tahun_terbit'
+                }
+            ]
+        });
+    }
+
+    $('#filter').click(function(){
+        var filter_penulis = $('#filter_penulis').val();
+        var filter_terbit = $('#filter_terbit').val();
+
+        if(filter_penulis != '' &&  filter_terbit != '')
+        {
+            $('#data_buku').DataTable().destroy();
+            fill_datatable(filter_penulis, filter_terbit);
+        }
+        else
+        {
+            alert('Harus memilih opsi keduanya!');
+        }
+    });
+
+    $('#reset').click(function(){
+        $('#filter_penulis').val('');
+        $('#filter_terbit').val('');
+        $('#data_buku').DataTable().destroy();
+        fill_datatable();
+    });
+
+});
+</script>
